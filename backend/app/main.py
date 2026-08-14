0fa105afe69fa75682647d9bd6d8318a23163671
malayjoshi01 <trailmscitp1@gmail.com>
2026-08-14 18:58:54 +0530

    Change CORS settings for FastAPI application
    Updated CORS middleware to allow a new frontend URL.

--- backend/app/main.py
+++ backend/app/main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:5173",
-        "https://frontend-five-omega-53.vercel.app/",
+        "https://taskflow-theta-puce.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
